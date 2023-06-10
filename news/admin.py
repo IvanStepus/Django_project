4ccabe8fc83b473c7d8cbb7ebeb8a7f210730fe1
IvanStepus <vanya-stepys@mail.ru>
2023-06-10 21:21:33 +0300

    test: linter was added

--- news/admin.py
+++ news/admin.py
@@ -4,7 +4,14 @@ from news.models import News, Comment
 
 admin.site.register(Comment)
 
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title','create_at', 'changed_at', 'published_at', 'published']
-    list_filter = ['title','create_at', 'published_at']
+    list_display = ['title',
+                    'create_at',
+                    'changed_at',
+                    'published_at',
+                    'published']
+    list_filter = ['title',
+                   'create_at',
+                   'published_at']
